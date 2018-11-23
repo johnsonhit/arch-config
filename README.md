02cbcb855e65f5ef4580442df0adc0636991913a
TsReaper <tsreaper@163.com>
2018-11-23 23:51:19 +0800

    Update config. Add screenshots.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Config files of my Arch Linux.
 
 For detailed description see [this blog](https://www.cnblogs.com/tsreaper/p/arch-config.html) (in Chinese).
+
+## Screenshot
+
+![screenshot1.png](screenshot/screenshot1.png?raw=true "Screenshot1")
+
+![screenshot2.png](screenshot/screenshot2.png?raw=true "Screenshot2")
+
+The artist of this cool [wall paper](https://www.pixiv.net/member_illust.php?mode=medium&illust_id=51677182) is [五色](https://www.pixiv.net/member.php?id=169533).
--- Tools/wifi.sh
+++ Tools/wifi.sh
@@ -1,41 +1,86 @@
 #!/usr/bin/env bash
 
-if [[ -z $2 ]]
-then
-    echo '''Usage: ./wifi.sh interface ssid [options]
+USAGE='''Usage: ./wifi.sh interface ssid [options]
 Options:
+  -w
+    Connect to a wireless network with WPA.
   -d
     Run dhcpcd after connection.'''
-    exit 1
-fi
 
-best_bss=""
-best_signal="-10000.00"
+interface=''
+ssid=''
+wpa=false
+dhcp=false
 
-echo "Trying to connect to $2"
-
-while read -r line
+while [ $# -ne 0 ]
 do
-    case $line in
-        *BSS*)
-            bss=`echo $line | cut -f2 -d' ' | cut -f1 -d'('`
+    case "$1" in
+        -w)
+            wpa=true
             ;;
-        *signal*)
-            signal=`echo $line | cut -f2 -d' '`
+        -d)
+            dhcp=true
             ;;
-        *$2*)
-            if [ `echo "$signal > $best_signal" | bc` -eq 1 ]
+        *)
+            if [ -z "$interface" ]
             then
-                best_signal=$signal
-                best_bss=$bss
+                interface="$1"
+            elif [ -z "$ssid" ]
+            then
+                ssid="$1"
+            else
+                echo "$USAGE"
+                exit 1
             fi
             ;;
     esac
-done <<< `ip link set $1 up && iw dev $1 scan | grep "^BSS\|signal\|$2"`
+    shift
+done
 
-echo "Connecting to ap $best_bss with signal $best_signal mdB"
-iwconfig $1 essid $2 ap $best_bss
-if [[ $3 = "-d" ]]
+if [ -z "$interface" ] || [ -z "$ssid" ]
 then
-    dhcpcd $1
+    echo "$USAGE"
+    exit 1
+fi
+
+echo "Trying to connect to $ssid"
+
+if [ $wpa == true ]
+then
+    read -s -p 'WPA Password: ' pass
+    echo ''
+    filename=/tmp/"wpa_passphrase_$ssid"
+    wpa_passphrase "$ssid" "$pass" > "$filename"
+    wpa_supplicant -B -i "$interface" -c "$filename"
+    rm "$filename"
+else
+    best_bss=''
+    best_signal='-10000.00'
+
+    while read -r line
+    do
+        case $line in
+            *BSS*)
+                bss=`echo $line | cut -f2 -d' ' | cut -f1 -d'('`
+                ;;
+            *signal*)
+                signal=`echo $line | cut -f2 -d' '`
+                ;;
+            *$2*)
+                if [ `echo "$signal > $best_signal" | bc` -eq 1 ]
+                then
+                    best_signal=$signal
+                    best_bss=$bss
+                fi
+                ;;
+        esac
+    done <<< `ip link set "$interface" up && iw dev "$interface" scan | grep "^BSS\|signal\|$ssid"`
+
+    echo "Connecting to AP $best_bss with signal $best_signal mdB"
+    iwconfig $interface essid $ssid ap $best_bss
+fi
+
+if [ $dhcp == true ]
+then
+    dhcpcd "$interface"
 fi
